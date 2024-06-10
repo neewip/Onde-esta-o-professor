fd4639347e07213123c883b67cbdd4c3f8488792
ponterror22 <viniponte2006@gmail.com>
2024-06-10 14:18:03 -0300

    frontend pagina principal

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.tudo}>
+     <View style={styles.cabecalho}> Onde está o professor? </View>
+     
+     <View style={styles.fundo}>
+     <Image
+    source={require("./assets/professor.png")}
+style={styles.imagem}
+    >
+      </Image>
+
+      <Image
+    source={require("./assets/salas.png")}
+style={styles.imagem}
+    >
+      </Image>
+
+      <Button
+        title="Pesquisar Produto"
+        onPress={() => navigation.navigate('Search')}
+      />
+
+</View>
+
+<Button style={styles.botoes}> teste</Button>
+
+
+<View style={styles.fundo2}>
+     <Image
+    source={require("./assets/alocacoes.png")}
+style={styles.imagem}
+    >
+      </Image>
+
+      <Image
+    source={require("./assets/visoes.png")}
+style={styles.imagem}
+    >
+      </Image>
+
+</View>
+
       <StatusBar style="auto" />
-    </View>
+       
+     </View>
+
   );
 }
 
@@ -16,5 +57,45 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  cabecalho: {
+    backgroundColor: 'black',
+    width: 400,
+    height: 150,
+    color: 'white',
+    alignItems: 'center',
+    fontSize: 30,
+    justifyContent:'center',
+    fontFamily: 'georgia'
+  },
+  tudo:{
+    backgroundColor:'#a0aecd',
+  },
+  imagem:{
+    width: 150,
+    height: 150,
+  },
+  texto:{
+  fontSize: 20,
+  color: 'white',
+  justifyContent:'flex-end',
+  display: 'flex',
+  },
+  fundo:{
+    display: 'flex',
+    justifyContent:'space-between',
+    flexDirection:'row',
+    marginLeft:25,
+    marginRight: 25,
+    marginTop:25
+  },
+
+  fundo2:{
+    display: 'flex',
+    justifyContent:'space-between',
+    flexDirection:'row',
+    marginLeft:25,
+    marginRight: 25,
+    marginTop:100
   },
 });
