de41763fc6ec3f25dce8ca4f4e2d8deaf0b3fe92
marianoisabela <marianoisabela834@gmail.com>
2024-06-11 08:10:29 -0300

    nav feito

--- frontend/App.js
+++ frontend/App.js
@@ -1,98 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
-
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen';
+import Professor from './Screens/ProfessorComponent';
+import Sala from './Screens/SalaComponent';
+import Alocacao from './Screens/AlocacaoComponent';
+import Visao from './Screens/VisaoComponent';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.tudo}>
-     <View style={styles.cabecalho}> Onde está o professor? </View>
-
-<View style={styles.fundo}>
-     <Image
-    source={require("./assets/professores.png")}
-    style={styles.imagem}
-    >
-      </Image>
-
-      <Image
-    source={require("./assets/salas.png")}
-    style={styles.imagem}
-    >
-      </Image>
-</View>
-
-
-<View style={styles.fundo2}>
-     <Image
-    source={require("./assets/alocacoes.png")}
-    style={styles.imagem}
-    >
-      </Image>
-
-      <Image
-    source={require("./assets/interrogacao.png")}
-    style={styles.imagem}
-    >
-      </Image>
-</View>
-
-
-      <StatusBar style="auto" />
-       
-     </View>
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Professor" component={Professor} />
+        <Stack.Screen name="Sala" component={Sala} />
+        <Stack.Screen name="Alocacao" component={Alocacao} />
+        <Stack.Screen name="Visao" component={Visao} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  cabecalho: {
-    backgroundColor: 'black',
-    width: 400,
-    height: 150,
-    color: 'white',
-    alignItems: 'center',
-    fontSize: 30,
-    justifyContent:'center',
-    fontFamily: 'georgia'
-  },
-  tudo:{
-    backgroundColor:'#a0aecd'
-  },
-
-  imagem: {
-   width:150,
-  height:150,
-  justifyContent: 'flex-end',
-
-  },
-
-  fundo:{
-    display: 'flex',
-    justifyContent:'space-between',
-    flexDirection:'row',
-    marginLeft:25,
-    marginRight: 25,
-    marginTop:25
-  },
-
-  fundo2:{
-    display: 'flex',
-    justifyContent:'space-between',
-    flexDirection:'row',
-    marginLeft:25,
-    marginRight: 25,
-    marginTop:100
-  }
-
-
-});
